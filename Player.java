2474b701f3f6dc48e221b9e0729a7b87376a96a9
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-07-27 13:30:29 +0530

    matchScore data member added to player

--- Player.java
+++ Player.java
@@ -3,16 +3,26 @@ package TennisScoring;
 public class Player {
 	private int setScore;
 	private int gameScore;
+	private int matchScore;
 	private String name;
 
 	public Player(String name) {
 		this.name = name;
 		this.setScore = 0;
 		this.gameScore = 0;
+		this.matchScore = 0;
 	}
 
 	public int getSetScore() {
 		return setScore;
+	}
+
+	public int getMatchScore() {
+		return matchScore;
+	}
+
+	public void setMatchScore(int matchScore) {
+		this.matchScore = matchScore;
 	}
 
 	public int getGameScore() {
