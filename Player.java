9eaa7a0f38b2c32f72d34620d4eb5712adbe2a13
AishwaryaRK <aishwarya.kaneri@gmail.com>
2015-07-27 11:09:29 +0530

    setters added to player

--- Player.java
+++ Player.java
@@ -4,16 +4,26 @@ public class Player {
 	private int setScore;
 	private int gameScore;
 	private String name;
+
 	public int getSetScore() {
 		return setScore;
 	}
+
 	public int getGameScore() {
 		return gameScore;
 	}
+
 	public String getName() {
 		return name;
 	}
+
+	public void setSetScore(int setScore) {
+		this.setScore = setScore;
+	}
+
+	public void setGameScore(int gameScore) {
+		this.gameScore = gameScore;
+	}
 	
-	
-	
+
 }
