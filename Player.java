71c36a056f6c5e091fe92d46d90c42907f5aa6b5
Srravya <srravya.c@gmail.com>
2015-07-27 11:07:27 +0530

    Add Contructor

--- Player.java
+++ Player.java
@@ -5,5 +5,15 @@ public class TennisPlayer {
 	private int gameScore;
 	private String name;
 	
+	public TennisPlayer() {
+	
+		super();
+		this.gameStatus = "";
+		this.team1Score = 0;
+		this.team2Score = 0;
+		this.team1SetScore = 0;
+		this.team2SetScore = 0;
+
+	}
 	
 }
